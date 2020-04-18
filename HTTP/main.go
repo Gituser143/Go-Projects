f6f13759750c67f15a8f5cff6188db23a0b325f5
Gituser143 <bhargavsnv100@gmail.com>
2020-04-18 15:37:14 +0530

    Add basic code to read body from GET requests

--- HTTP/main.go
+++ HTTP/main.go
@@ -8,8 +8,18 @@ import (
 
 func main() {
 	resp, err := http.Get("http://google.com")
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(resp)
+
+	body := make([]byte, 100000)
+
+	n, err2 := resp.Body.Read(body)
+	if n == 0 && err2 != nil {
+		log.Fatal(err2)
+	}
+
+	str := string(body)
+	fmt.Println(str)
 }
