30cb65ed98c3f2c93c3bd602ec58a6e30dadd513
Gituser143 <bhargavsnv100@gmail.com>
2020-04-19 15:11:04 +0530

    Add function to check website status

--- Channels/main.go
+++ Channels/main.go
@@ -2,22 +2,24 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net/http"
-	"os"
 )
 
 func main() {
 	websites := []string{"http://google.com", "http://facebook.com", "http://golang.org", "http://amazon.com"}
 
 	for _, url := range websites {
-		resp, err := http.Get(url)
+		err := checkUrlStatus(url)
 
 		if err != nil {
-			fmt.Println("Error: Could not fetch", url)
+			fmt.Println("The Website", url, "is down")
 		} else {
-			fmt.Println("\n======================\n", url, "\n======================\n ")
-			io.Copy(os.Stdout, resp.Body)
+			fmt.Println(url, "is up and running")
 		}
 	}
+}
+
+func checkUrlStatus(url string) error {
+	_, err := http.Get(url)
+	return err
 }
